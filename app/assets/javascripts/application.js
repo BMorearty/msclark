6b2bdebf90ee024a90e37a5a09160bece18b5341
Brian Morearty <brian@morearty.org>
2013-03-13 16:22:26 -0700

    Add jquery-ui to demonstrate how turbolinks speeds things up.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require js/jquery-ui-1.10.1.custom.min
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Msclark
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.paths << Rails.root.join("vendor", "assets", "jquery-ui")
   end
 end
